797abb92a72076043985121058a0e8b2dd48af8e
DentVega <dentvega6@gmail.com>
2019-11-28 00:43:12 -0400

    - Login Widget

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intimate/src/widgets/logo_widget.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,15 +7,94 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _name = '';
+  String _nickname = '';
+  String _email = '';
+  String _numberPhone = '';
+  String _password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Intimos'),
       ),
-      body: Center(
-        child: Text('Login'),
+      body: ListView(
+        children: <Widget>[
+          LogoWidget(),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text('Registro'),
+            ),
+          ),
+          _createEmail(),
+          Divider(),
+          _createPassword(),
+          _createButtonLogin(),
+          _createSignupButton(),
+        ],
       ),
     );
   }
+
+  Widget _createEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          hintText: 'Email de la persona',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)),
+      onChanged: (valor) {
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _createPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: 'Contraseña',
+          labelText: 'Contraseña',
+          suffixIcon: Icon(Icons.lock),
+          icon: Icon(Icons.lock)),
+      onChanged: (value) {
+        setState(() {
+          _password = value;
+        });
+      },
+    );
+  }
+
+  Widget _createButtonLogin() {
+    return Container(
+      padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+      child: Center(
+        child: RaisedButton(
+          child: Text('Iniciar sesion'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _iniciarSession(),
+        ),
+      ),
+    );
+  }
+
+  Widget _createSignupButton() {
+    return Container(
+      child: FlatButton(
+        child: Text('Registrar'),
+        onPressed: () => goSignup(),
+      ),
+    );
+  }
+
+  _iniciarSession() {}
+
+  goSignup() {}
 }
--- lib/src/widgets/logo_widget.dart
+++ lib/src/widgets/logo_widget.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class LogoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Image(
+        image: AssetImage('assets/img/logo_capi.png'),
+      ),
+    );
   }
 }
