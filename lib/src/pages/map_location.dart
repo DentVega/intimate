659694550af6c5fe259c4e75e6a522f969d057f8
DentVega <dentvega6@gmail.com>
2020-02-02 22:25:02 -0400

    Ubicacion del usuario

--- lib/src/pages/map_location.dart
+++ lib/src/pages/map_location.dart
@@ -3,9 +3,11 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:intimate/src/model/churches_model.dart';
 import 'package:intimate/src/providers/cloud_firestore_api.dart';
 import 'package:latlong/latlong.dart';
+import 'package:user_location/user_location.dart';
 
 //https://pub.dev/packages/flutter_map#-installing-tab-
 //https://account.mapbox.com/
+//https://pub.dev/packages/flutter_map
 
 class MapLocation extends StatefulWidget {
   @override
@@ -14,32 +16,69 @@ class MapLocation extends StatefulWidget {
 
 class _MapLocationState extends State<MapLocation> {
   final map = new MapController();
+  UserLocationOptions userLocationOptions;
 
   String tipoMapa = 'streets';
   final db = CloudFirestoreAPI();
   final positionCapi = LatLng(
       double.parse('-17.374314585719535'), double.parse('-66.13140478571779'));
   BuildContext contextApp;
+  List<Marker> markers = [];
 
   @override
   Widget build(BuildContext context) {
     contextApp = context;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mapa de la comunidad'),
+        title: Text('Mapa'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.map),
+              onPressed: () {
+                setState(() {
+                  if (tipoMapa == 'streets') {
+                    tipoMapa = 'dark';
+                  } else if (tipoMapa == 'dark') {
+                    tipoMapa = 'light';
+                  } else if (tipoMapa == 'light') {
+                    tipoMapa = 'outdoors';
+                  } else if (tipoMapa == 'outdoors') {
+                    tipoMapa = 'satellite';
+                  } else {
+                    tipoMapa = 'streets';
+                  }
+                });
+              })
+        ],
       ),
       body: Stack(
         children: <Widget>[_crearFlutterMap(), _createListLocations()],
       ),
-      floatingActionButton: _crearBotonFlotante(context),
+    );
+  }
+
+  void userLocation() {
+    userLocationOptions = UserLocationOptions(
+      context: context,
+      showMoveToCurrentLocationFloatingActionButton: true,
+      verbose: true,
+      mapController: map,
+      markers: markers,
     );
   }
 
   Widget _crearFlutterMap() {
+    userLocation();
     return FlutterMap(
       mapController: map,
-      options: new MapOptions(center: positionCapi, zoom: 15.00),
-      layers: [_crearMapa(), _crearMarcadores()],
+      options: new MapOptions(center: positionCapi, zoom: 15.00, plugins: [
+        UserLocationPlugin(),
+      ]),
+      layers: [
+        _crearMapa(),
+        MarkerLayerOptions(markers: markers),
+        userLocationOptions
+      ],
     );
   }
 
@@ -53,45 +92,6 @@ class _MapLocationState extends State<MapLocation> {
           'id': 'mapbox.$tipoMapa'
           // streets, dark, light, outdoors, satellite
         });
-  }
-
-  _crearMarcadores() {
-    return MarkerLayerOptions(markers: <Marker>[
-      Marker(
-          width: 100.0,
-          height: 100.0,
-          point: positionCapi,
-          builder: (context) => Container(
-                child: Icon(
-                  Icons.location_on,
-                  size: 45.0,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ))
-    ]);
-  }
-
-  Widget _crearBotonFlotante(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {
-        // streets, dark, light, outdoors, satellite
-        setState(() {
-          if (tipoMapa == 'streets') {
-            tipoMapa = 'dark';
-          } else if (tipoMapa == 'dark') {
-            tipoMapa = 'light';
-          } else if (tipoMapa == 'light') {
-            tipoMapa = 'outdoors';
-          } else if (tipoMapa == 'outdoors') {
-            tipoMapa = 'satellite';
-          } else {
-            tipoMapa = 'streets';
-          }
-        });
-      },
-      child: Icon(Icons.repeat),
-      backgroundColor: Theme.of(context).primaryColor,
-    );
   }
 
   Widget _createListLocations() {
@@ -122,10 +122,29 @@ class _MapLocationState extends State<MapLocation> {
     }
   }
 
+  addMarkers(List<Church> churles) {
+    churles.forEach((church) {
+      markers.add(Marker(
+          width: 100.0,
+          point: LatLng(church.location.latitude, church.location.longitude),
+          height: 100.0,
+          builder: (context) => Container(
+            child: Icon(
+              Icons.location_on,
+              size: 45.0,
+              color: Theme.of(context).primaryColor,
+            ),
+          )));
+    });
+    print('markes: ${markers}');
+    print('markes: ${markers.length}');
+  }
+
   Widget _location(Church church) {
     return GestureDetector(
-      onTap: (){
-        LatLng position = LatLng(church.location.latitude, church.location.longitude);
+      onTap: () {
+        LatLng position =
+            LatLng(church.location.latitude, church.location.longitude);
         map.move(position, 15.0);
       },
       child: Container(
@@ -134,9 +153,16 @@ class _MapLocationState extends State<MapLocation> {
           elevation: 5.0,
           child: Row(
             children: <Widget>[
-              SizedBox(width: 10.0,),
-              Icon(Icons.home, color: Colors.red,),
-              SizedBox(width: 5.0,),
+              SizedBox(
+                width: 10.0,
+              ),
+              Icon(
+                Icons.home,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 5.0,
+              ),
               Text(church.name)
             ],
           ),
